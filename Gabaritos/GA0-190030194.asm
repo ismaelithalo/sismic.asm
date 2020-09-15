d43e2e07ef962a590a1403ed2a689294ffc9e6ab
ismaelithalo <ismaelithalo@gmail.com>
2020-09-14 23:10:22 -0300

    Gabarito da prova A0

---
+++ Gabaritos/GA0-190030194.asm
@@ -0,0 +1,48 @@
+; 190030194 - Ismael Ithalo Barbosa Pinto
+;-------------------------------------------------------------------------------
+; Main loop here
+;-------------------------------------------------------------------------------
+A0:       MOV   #COVID_MM7, R4
+          MOV   #COVID_MAR,R5
+          CALL  #MAR_MM7
+          JMP   $
+          NOP
+
+; SUA SOLUÇÃO
+MAR_MM7:
+          mov.b #31, R6; março
+
+loop_vec: mov.w  0(R5), R10
+          add.w -2(R5), R10
+          add.w -4(R5), R10
+          add.w -6(R5), R10
+          add.w -8(R5), R10
+          add.w -10(R5), R10
+          add.w -12(R5), R10
+		      clr.w R11
+div:
+          sub.w #7, R10
+          inc  R11
+          cmp  #7, R10
+          jc  div
+
+          mov.w R11, 0(R4)
+          incd R5
+          incd R4
+          dec.b R6
+          jnz loop_vec
+          ret
+                  
+             .data
+; Casos nos últimos 6 dias de fevereiro
+COVID_FEV:   .word     0,    0,    1,    1,    1,    2
+;
+; Casos durante os 31 dias se março
+COVID_MAR:   .word     2,    2,    2,    3,    7,   13,   19,   25,  25,    34
+             .word    52,   77,   98,  121,  200,  234,  291,  428,  621,  904
+             .word  1128, 1546, 1891, 2201, 2433, 2915, 3417, 3903, 4256, 4579, 5717
+;
+; Média móvel de março (sua resposta)
+COVID_MM7:   .word    0,     0,    0,    0,    0,    0,    0,    0,    0,    0
+             .word    0,     0,    0,    0,    0,    0,    0,    0,    0,    0
+             .word    0,     0,    0,    0,    0,    0,    0,    0,    0,    0,    0
